7b3bc1b922fc14519c9a022be569cbc3bfe4ae0f
Johan Eliasson <johan@eliasson.me>
2023-01-07 19:48:09 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,298 @@
+import * as serializerPb from "./proto_stubs/serializer/serializer_pb";
+import * as apiRestPb from "./proto_stubs/api/item_cr_0v1_pb";
+import fetch from "cross-fetch";
+
+const DB1_API_ITEM_GET_URL = "https://item.db1.io/get";
+const DB1_API_ITEM_CREATE_URL = "https://item.db1.io/create";
+const DB1_API_ITEM_SET_URL = "https://item.db1.io/set";
+
+async function makeHttpRequest(url: string, data: any): Promise<any> {
+  const httpResponse = await fetch(url, {
+    method: "POST",
+    body: data,
+    // mode: 'cors',
+    headers: { "content-type": "application/octet-stream" },
+  });
+  return httpResponse;
+}
+
+function check_common_status(common: any) {
+  if (common == undefined || common == null) {
+    throw new Error("No common response");
+  }
+  if (common.status == undefined || common.status == null) {
+    throw new Error("No common response status");
+  }
+  if (
+    common.status == apiRestPb.item_cr_0v1.CommonResponse.Status.INTERNAL_ERROR
+  ) {
+    throw new Error("Internal error");
+  }
+  if (
+    common.status == apiRestPb.item_cr_0v1.CommonResponse.Status.BAD_REQUEST
+  ) {
+    throw new Error("Bad request");
+  }
+  if (common.status == apiRestPb.item_cr_0v1.CommonResponse.Status.OK_REQUEST) {
+    return;
+  }
+}
+
+async function get_value_and_metadata(resource_id: string) {
+  let common_request = new apiRestPb.item_cr_0v1.CommonRequest();
+  // common_request.token = "";
+  // common_request.usage_message = "";
+
+  const request = apiRestPb.item_cr_0v1.GetRequest.create({
+    common: common_request,
+    resourceId: resource_id,
+    withoutMetavaribles: false,
+    withoutItemValue: false,
+    maxSizeBytes: null,
+  });
+
+  const encoded_request =
+    apiRestPb.item_cr_0v1.GetRequest.encode(request).finish();
+  const http_response = await makeHttpRequest(
+    DB1_API_ITEM_GET_URL,
+    encoded_request
+  );
+
+  const array_buffer = await http_response.arrayBuffer();
+  const response = apiRestPb.item_cr_0v1.GetResponse.decode(
+    new Uint8Array(array_buffer)
+  );
+
+  check_common_status(response.common);
+  if (response.status == apiRestPb.item_cr_0v1.GetResponse.Status.NOT_FOUND) {
+    throw new Error("Not found");
+  }
+  if (response.status == apiRestPb.item_cr_0v1.GetResponse.Status.FOUND) {
+  }
+
+  let meta_variables: any = {};
+  for (const i in response.metavaribles) {
+    const meta_variable = response.metavaribles[i];
+    if (
+      meta_variable.key == undefined ||
+      meta_variable.key == null ||
+      meta_variable.value == undefined ||
+      meta_variable.value == null
+    ) {
+      throw new Error("Bad meta variable");
+    }
+    meta_variables[meta_variable.key] = meta_variable.value;
+  }
+  meta_variables["size_bytes"] = response.sizeBytes;
+  meta_variables["created_ms"] = response.createdMs;
+  meta_variables["updated_ms"] = response.updatedMs;
+
+  return {
+    value: serializerPb.Value.decode(response.itemValue),
+    meta_variables: meta_variables,
+  };
+}
+
+export const PROTO_TYPES_TO_JS_TYPES = {
+  INT64_: "int",
+  FLOAT_: "float",
+  STRING_: "string",
+  BOOL_: "bool",
+  BYTES_: "bytes",
+  LIST: "list",
+  DICT: "dict",
+  NDARRAY: "ndarray",
+  DATAFRAME: "dataframe",
+};
+
+export function get_type_from_proto(proto_value: any): string {
+  return serializerPb.Value.Type[proto_value.type];
+}
+
+export function get_value_description_from_proto(
+  proto_value: any,
+  max_length?: number
+): string {
+  if (max_length == undefined) {
+    max_length = 80;
+  }
+  const type = get_type_from_proto(proto_value);
+
+  if (type == "INT64_") {
+    return String(proto_value.int64_);
+  } else if (type == "FLOAT_") {
+    return String(proto_value.float_);
+  } else if (type == "STRING_") {
+    if (proto_value.string_.length > max_length) {
+      return proto_value.string_.substr(0, max_length) + " ...";
+    } else {
+      return proto_value.string_;
+    }
+  } else if (type == "BOOL_") {
+    return String(proto_value.bool_);
+  } else if (type == "BYTES_") {
+    return "";
+  } else if (type == "LIST") {
+    return "Length: " + Object.keys(proto_value.list).length;
+  } else if (type == "DICT") {
+    // let desc = "Keys: " + JSON.stringify(proto_value.dict.keys);
+    let desc = "Keys: [" + proto_value.dict.keys.join(", ") + "]";
+    if (desc.length > max_length) {
+      desc = "Length: " + proto_value.dict.keys.length;
+    }
+    return desc;
+    // } else if (type == "NDARRAY") {
+    //   return (
+    //     "Shape: [" +
+    //     proto_value.ndarray.shape.join(", ") +
+    //     "] | Type: " +
+    //     proto_value.ndarray.type
+    //   );
+    // } else if (type == "DATAFRAME") {
+    //   const table = tableFromIPC(proto_value.bytes_);
+    //   let desc =
+    //     "Columns: [" +
+    //     table.schema.fields.map((field) => field.name).join(", ") +
+    //     "]";
+    //   if (desc.length > max_length) {
+    //     desc = "Columns num: " + table.schema.fields.length;
+    //   }
+    //   return desc;
+  } else {
+    throw new Error("Could not parse json value.");
+  }
+}
+
+export async function set_value(resource_id: string, value: any) {
+  let common_request = new apiRestPb.item_cr_0v1.CommonRequest();
+  // common_request.token = "";
+  // common_request.usage_message = "";
+  const ser_val = serialize_element(value);
+
+  const request = apiRestPb.item_cr_0v1.SetRequest.create({
+    common: common_request,
+    resourceId: resource_id,
+    itemValue: ser_val,
+  });
+
+  const encoded_request =
+    apiRestPb.item_cr_0v1.SetRequest.encode(request).finish();
+  const http_response = await makeHttpRequest(
+    DB1_API_ITEM_SET_URL,
+    encoded_request
+  );
+
+  const array_buffer = await http_response.arrayBuffer();
+  const response = apiRestPb.item_cr_0v1.SetResponse.decode(
+    new Uint8Array(array_buffer)
+  );
+
+  check_common_status(response.common);
+  if (response.status == apiRestPb.item_cr_0v1.SetResponse.Status.NOT_FOUND) {
+    throw new Error("Not found");
+  }
+  if (response.status == apiRestPb.item_cr_0v1.SetResponse.Status.UPDATED) {
+    return;
+  }
+}
+
+export function get_element_type(element: any): string {
+  if (typeof element == "number" && Number.isInteger(element)) {
+    return "int";
+  }
+  if (typeof element == "number") {
+    return "float";
+  }
+  if (typeof element == "string") {
+    return "string";
+  }
+  if (typeof element == "boolean") {
+    return "bool";
+  }
+  // if (typeof element == "object" && element instanceof Table) { return "data_frame"; }
+  // if (typeof element == "object" && element.constructor == Uint8Array) { return "bytes"; }
+  // if (typeof element == "object" && element.constructor == Array) { return "list"; }
+  // if (typeof element == "object" && "proto_type" in element && element["proto_type"] == "ndarray") { return "ndarray"; }
+  // if (typeof element == "object") { return "dict"; }
+  throw new Error(`Element of type '${typeof element}' could classified.`);
+}
+
+export function serialize_element(js_element: any): Uint8Array {
+  const pb_element = js_to_pb_element(js_element);
+  const ser_element = serializerPb.Value.encode(pb_element).finish();
+  return ser_element;
+}
+
+function js_to_pb_element(js_element: any): serializerPb.Value {
+  let pb_element = serializerPb.Value.create();
+  const element_type = get_element_type(js_element);
+
+  if (element_type == "int") {
+    pb_element.type = serializerPb.Value.Type.INT64_;
+    pb_element.int64_ = js_element;
+  } else if (element_type == "float") {
+    pb_element.type = serializerPb.Value.Type.FLOAT_;
+    pb_element.float_ = js_element;
+  } else if (element_type == "string") {
+    pb_element.type = serializerPb.Value.Type.STRING_;
+    pb_element.string_ = js_element;
+  } else if (element_type == "bool") {
+    pb_element.type = serializerPb.Value.Type.BOOL_;
+    pb_element.bool_ = js_element;
+  } else if (element_type == "bytes") {
+    pb_element.type = serializerPb.Value.Type.BYTES_;
+    pb_element.bytes_ = js_element;
+  } else if (element_type == "list") {
+    pb_element.type = serializerPb.Value.Type.LIST;
+    for (const i in js_element) {
+      pb_element.list.push(js_to_pb_element(js_element[i]));
+    }
+  } else if (element_type == "ndarray") {
+    pb_element.type = serializerPb.Value.Type.NDARRAY;
+    const param: any = {
+      bytes: js_element.bytes,
+      shape: js_element.shape,
+      type: js_element.type,
+    };
+    let ndarray = serializerPb.NDArray.create(param);
+    pb_element.ndarray = ndarray;
+  } else if (element_type == "dict") {
+    pb_element.type = serializerPb.Value.Type.DICT;
+    let keys = [];
+    let values = [];
+    for (const key in js_element) {
+      keys.push(key);
+      values.push(js_to_pb_element(js_element[key]));
+    }
+    let dict_ = serializerPb.Dict.create({ keys: keys, values: values });
+    pb_element.dict = dict_;
+  } else {
+    throw new Error(
+      `Element of type '${typeof js_element}' could not be serialized.`
+    );
+  }
+
+  return pb_element;
+}
+
+async function get(key: string): Promise<any> {
+  const result = await get_value_and_metadata(key);
+  const value = get_value_description_from_proto(result.value);
+  return value;
+}
+
+async function set(key: string, value: any) {
+  await set_value(key, value);
+}
+
+async function main() {
+  const key = "test-str";
+
+  await set(key, "lolnice");
+
+  const value = await get(key);
+
+  console.log(value);
+}
+
+main();
---
+++ src/proto_stubs/serializer/serializer_pb.d.ts
@@ -0,0 +1,531 @@
+import * as $protobuf from "protobufjs";
+import Long = require("long");
+/** Properties of a Value. */
+export interface IValue {
+  /** Value type */
+  type?: Value.Type | null;
+
+  /** Value int64_ */
+  int64_?: number | Long | null;
+
+  /** Value float_ */
+  float_?: number | null;
+
+  /** Value string_ */
+  string_?: string | null;
+
+  /** Value bool_ */
+  bool_?: boolean | null;
+
+  /** Value bytes_ */
+  bytes_?: Uint8Array | null;
+
+  /** Value list */
+  list?: IValue[] | null;
+
+  /** Value dict */
+  dict?: IDict | null;
+
+  /** Value ndarray */
+  ndarray?: INDArray | null;
+
+  /** Value dataFrame */
+  dataFrame?: IDataFrame | null;
+}
+
+/** Represents a Value. */
+export class Value implements IValue {
+  /**
+   * Constructs a new Value.
+   * @param [properties] Properties to set
+   */
+  constructor(properties?: IValue);
+
+  /** Value type. */
+  public type: Value.Type;
+
+  /** Value int64_. */
+  public int64_: number | Long;
+
+  /** Value float_. */
+  public float_: number;
+
+  /** Value string_. */
+  public string_: string;
+
+  /** Value bool_. */
+  public bool_: boolean;
+
+  /** Value bytes_. */
+  public bytes_: Uint8Array;
+
+  /** Value list. */
+  public list: IValue[];
+
+  /** Value dict. */
+  public dict?: IDict | null;
+
+  /** Value ndarray. */
+  public ndarray?: INDArray | null;
+
+  /** Value dataFrame. */
+  public dataFrame?: IDataFrame | null;
+
+  /**
+   * Creates a new Value instance using the specified properties.
+   * @param [properties] Properties to set
+   * @returns Value instance
+   */
+  public static create(properties?: IValue): Value;
+
+  /**
+   * Encodes the specified Value message. Does not implicitly {@link Value.verify|verify} messages.
+   * @param message Value message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encode(
+    message: IValue,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Encodes the specified Value message, length delimited. Does not implicitly {@link Value.verify|verify} messages.
+   * @param message Value message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encodeDelimited(
+    message: IValue,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Decodes a Value message from the specified reader or buffer.
+   * @param reader Reader or buffer to decode from
+   * @param [length] Message length if known beforehand
+   * @returns Value
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decode(
+    reader: $protobuf.Reader | Uint8Array,
+    length?: number
+  ): Value;
+
+  /**
+   * Decodes a Value message from the specified reader or buffer, length delimited.
+   * @param reader Reader or buffer to decode from
+   * @returns Value
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decodeDelimited(reader: $protobuf.Reader | Uint8Array): Value;
+
+  /**
+   * Verifies a Value message.
+   * @param message Plain object to verify
+   * @returns `null` if valid, otherwise the reason why it is not
+   */
+  public static verify(message: { [k: string]: any }): string | null;
+
+  /**
+   * Creates a Value message from a plain object. Also converts values to their respective internal types.
+   * @param object Plain object
+   * @returns Value
+   */
+  public static fromObject(object: { [k: string]: any }): Value;
+
+  /**
+   * Creates a plain object from a Value message. Also converts values to other types if specified.
+   * @param message Value
+   * @param [options] Conversion options
+   * @returns Plain object
+   */
+  public static toObject(
+    message: Value,
+    options?: $protobuf.IConversionOptions
+  ): { [k: string]: any };
+
+  /**
+   * Converts this Value to JSON.
+   * @returns JSON object
+   */
+  public toJSON(): { [k: string]: any };
+
+  /**
+   * Gets the default type url for Value
+   * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+   * @returns The default type url
+   */
+  public static getTypeUrl(typeUrlPrefix?: string): string;
+}
+
+export namespace Value {
+  /** Type enum. */
+  enum Type {
+    NONE = 0,
+    INT64_ = 1,
+    FLOAT_ = 2,
+    STRING_ = 3,
+    BOOL_ = 4,
+    BYTES_ = 5,
+    LIST = 6,
+    DICT = 7,
+    NDARRAY = 8,
+    DATAFRAME = 9,
+  }
+}
+
+/** Properties of a NDArray. */
+export interface INDArray {
+  /** NDArray bytes_ */
+  bytes_?: Uint8Array | null;
+
+  /** NDArray shape */
+  shape?: number[] | null;
+
+  /** NDArray type */
+  type?: string | null;
+}
+
+/** Represents a NDArray. */
+export class NDArray implements INDArray {
+  /**
+   * Constructs a new NDArray.
+   * @param [properties] Properties to set
+   */
+  constructor(properties?: INDArray);
+
+  /** NDArray bytes_. */
+  public bytes_: Uint8Array;
+
+  /** NDArray shape. */
+  public shape: number[];
+
+  /** NDArray type. */
+  public type: string;
+
+  /**
+   * Creates a new NDArray instance using the specified properties.
+   * @param [properties] Properties to set
+   * @returns NDArray instance
+   */
+  public static create(properties?: INDArray): NDArray;
+
+  /**
+   * Encodes the specified NDArray message. Does not implicitly {@link NDArray.verify|verify} messages.
+   * @param message NDArray message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encode(
+    message: INDArray,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Encodes the specified NDArray message, length delimited. Does not implicitly {@link NDArray.verify|verify} messages.
+   * @param message NDArray message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encodeDelimited(
+    message: INDArray,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Decodes a NDArray message from the specified reader or buffer.
+   * @param reader Reader or buffer to decode from
+   * @param [length] Message length if known beforehand
+   * @returns NDArray
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decode(
+    reader: $protobuf.Reader | Uint8Array,
+    length?: number
+  ): NDArray;
+
+  /**
+   * Decodes a NDArray message from the specified reader or buffer, length delimited.
+   * @param reader Reader or buffer to decode from
+   * @returns NDArray
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decodeDelimited(reader: $protobuf.Reader | Uint8Array): NDArray;
+
+  /**
+   * Verifies a NDArray message.
+   * @param message Plain object to verify
+   * @returns `null` if valid, otherwise the reason why it is not
+   */
+  public static verify(message: { [k: string]: any }): string | null;
+
+  /**
+   * Creates a NDArray message from a plain object. Also converts values to their respective internal types.
+   * @param object Plain object
+   * @returns NDArray
+   */
+  public static fromObject(object: { [k: string]: any }): NDArray;
+
+  /**
+   * Creates a plain object from a NDArray message. Also converts values to other types if specified.
+   * @param message NDArray
+   * @param [options] Conversion options
+   * @returns Plain object
+   */
+  public static toObject(
+    message: NDArray,
+    options?: $protobuf.IConversionOptions
+  ): { [k: string]: any };
+
+  /**
+   * Converts this NDArray to JSON.
+   * @returns JSON object
+   */
+  public toJSON(): { [k: string]: any };
+
+  /**
+   * Gets the default type url for NDArray
+   * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+   * @returns The default type url
+   */
+  public static getTypeUrl(typeUrlPrefix?: string): string;
+}
+
+/** Properties of a Dict. */
+export interface IDict {
+  /** Dict keys */
+  keys?: string[] | null;
+
+  /** Dict values */
+  values?: IValue[] | null;
+}
+
+/** Represents a Dict. */
+export class Dict implements IDict {
+  /**
+   * Constructs a new Dict.
+   * @param [properties] Properties to set
+   */
+  constructor(properties?: IDict);
+
+  /** Dict keys. */
+  public keys: string[];
+
+  /** Dict values. */
+  public values: IValue[];
+
+  /**
+   * Creates a new Dict instance using the specified properties.
+   * @param [properties] Properties to set
+   * @returns Dict instance
+   */
+  public static create(properties?: IDict): Dict;
+
+  /**
+   * Encodes the specified Dict message. Does not implicitly {@link Dict.verify|verify} messages.
+   * @param message Dict message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encode(
+    message: IDict,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Encodes the specified Dict message, length delimited. Does not implicitly {@link Dict.verify|verify} messages.
+   * @param message Dict message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encodeDelimited(
+    message: IDict,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Decodes a Dict message from the specified reader or buffer.
+   * @param reader Reader or buffer to decode from
+   * @param [length] Message length if known beforehand
+   * @returns Dict
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decode(
+    reader: $protobuf.Reader | Uint8Array,
+    length?: number
+  ): Dict;
+
+  /**
+   * Decodes a Dict message from the specified reader or buffer, length delimited.
+   * @param reader Reader or buffer to decode from
+   * @returns Dict
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decodeDelimited(reader: $protobuf.Reader | Uint8Array): Dict;
+
+  /**
+   * Verifies a Dict message.
+   * @param message Plain object to verify
+   * @returns `null` if valid, otherwise the reason why it is not
+   */
+  public static verify(message: { [k: string]: any }): string | null;
+
+  /**
+   * Creates a Dict message from a plain object. Also converts values to their respective internal types.
+   * @param object Plain object
+   * @returns Dict
+   */
+  public static fromObject(object: { [k: string]: any }): Dict;
+
+  /**
+   * Creates a plain object from a Dict message. Also converts values to other types if specified.
+   * @param message Dict
+   * @param [options] Conversion options
+   * @returns Plain object
+   */
+  public static toObject(
+    message: Dict,
+    options?: $protobuf.IConversionOptions
+  ): { [k: string]: any };
+
+  /**
+   * Converts this Dict to JSON.
+   * @returns JSON object
+   */
+  public toJSON(): { [k: string]: any };
+
+  /**
+   * Gets the default type url for Dict
+   * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+   * @returns The default type url
+   */
+  public static getTypeUrl(typeUrlPrefix?: string): string;
+}
+
+/** Properties of a DataFrame. */
+export interface IDataFrame {
+  /** DataFrame index */
+  index?: IValue | null;
+
+  /** DataFrame columns */
+  columns?: IValue | null;
+
+  /** DataFrame values */
+  values?: IValue[] | null;
+}
+
+/** Represents a DataFrame. */
+export class DataFrame implements IDataFrame {
+  /**
+   * Constructs a new DataFrame.
+   * @param [properties] Properties to set
+   */
+  constructor(properties?: IDataFrame);
+
+  /** DataFrame index. */
+  public index?: IValue | null;
+
+  /** DataFrame columns. */
+  public columns?: IValue | null;
+
+  /** DataFrame values. */
+  public values: IValue[];
+
+  /**
+   * Creates a new DataFrame instance using the specified properties.
+   * @param [properties] Properties to set
+   * @returns DataFrame instance
+   */
+  public static create(properties?: IDataFrame): DataFrame;
+
+  /**
+   * Encodes the specified DataFrame message. Does not implicitly {@link DataFrame.verify|verify} messages.
+   * @param message DataFrame message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encode(
+    message: IDataFrame,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Encodes the specified DataFrame message, length delimited. Does not implicitly {@link DataFrame.verify|verify} messages.
+   * @param message DataFrame message or plain object to encode
+   * @param [writer] Writer to encode to
+   * @returns Writer
+   */
+  public static encodeDelimited(
+    message: IDataFrame,
+    writer?: $protobuf.Writer
+  ): $protobuf.Writer;
+
+  /**
+   * Decodes a DataFrame message from the specified reader or buffer.
+   * @param reader Reader or buffer to decode from
+   * @param [length] Message length if known beforehand
+   * @returns DataFrame
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decode(
+    reader: $protobuf.Reader | Uint8Array,
+    length?: number
+  ): DataFrame;
+
+  /**
+   * Decodes a DataFrame message from the specified reader or buffer, length delimited.
+   * @param reader Reader or buffer to decode from
+   * @returns DataFrame
+   * @throws {Error} If the payload is not a reader or valid buffer
+   * @throws {$protobuf.util.ProtocolError} If required fields are missing
+   */
+  public static decodeDelimited(
+    reader: $protobuf.Reader | Uint8Array
+  ): DataFrame;
+
+  /**
+   * Verifies a DataFrame message.
+   * @param message Plain object to verify
+   * @returns `null` if valid, otherwise the reason why it is not
+   */
+  public static verify(message: { [k: string]: any }): string | null;
+
+  /**
+   * Creates a DataFrame message from a plain object. Also converts values to their respective internal types.
+   * @param object Plain object
+   * @returns DataFrame
+   */
+  public static fromObject(object: { [k: string]: any }): DataFrame;
+
+  /**
+   * Creates a plain object from a DataFrame message. Also converts values to other types if specified.
+   * @param message DataFrame
+   * @param [options] Conversion options
+   * @returns Plain object
+   */
+  public static toObject(
+    message: DataFrame,
+    options?: $protobuf.IConversionOptions
+  ): { [k: string]: any };
+
+  /**
+   * Converts this DataFrame to JSON.
+   * @returns JSON object
+   */
+  public toJSON(): { [k: string]: any };
+
+  /**
+   * Gets the default type url for DataFrame
+   * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+   * @returns The default type url
+   */
+  public static getTypeUrl(typeUrlPrefix?: string): string;
+}

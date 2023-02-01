d198a989d6ceb154aec852f05003ea0ff7a47f69
Anton Eriksson <anton.emanuel@icloud.com>
2023-02-01 21:23:10 +0100

    Adds types utlities

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,16 +1,44 @@
 import { Table } from '@apache-arrow/es5-esm';
 
+import * as serializerPb from "../proto_stubs/serializer/serializer_pb";
+
+
+
+export enum ValueType {
+    INT = "int",
+    FLOAT = "float",
+    STRING = "string",
+    BOOL = "bool",
+    BYTES = "bytes",
+    LIST = "list",
+    DICT = "dict",
+    NDARRAY = "ndarray",
+    DATA_FRAME = "data_frame",
+}
+
+export const PROTO_TO_VALUE_TYPE = {
+    [serializerPb.Value.Type.INT64_]: ValueType.INT,
+    [serializerPb.Value.Type.FLOAT_]: ValueType.FLOAT,
+    [serializerPb.Value.Type.STRING_]: ValueType.STRING,
+    [serializerPb.Value.Type.BOOL_]: ValueType.BOOL,
+    [serializerPb.Value.Type.BYTES_]: ValueType.BYTES,
+    [serializerPb.Value.Type.LIST]: ValueType.LIST,
+    [serializerPb.Value.Type.DICT]: ValueType.DICT,
+    [serializerPb.Value.Type.NDARRAY]: ValueType.NDARRAY,
+    [serializerPb.Value.Type.DATAFRAME]: ValueType.DATA_FRAME,
+}
+
 
 export function getValueType(jsValue: any): string {
-    if (typeof jsValue == "number" && Number.isInteger(jsValue)) { return "int"; }
-    if (typeof jsValue == "number") { return "float"; }
-    if (typeof jsValue == "string") { return "string"; }
-    if (typeof jsValue == "boolean") { return "bool"; }
-    if (typeof jsValue == "object" && jsValue instanceof Table) { return "data_frame"; }
-    if (typeof jsValue == "object" && jsValue.constructor == Uint8Array) { return "bytes"; }
-    if (typeof jsValue == "object" && jsValue.constructor == Array) { return "list"; }
-    if (typeof jsValue == "object" && "__value_type__" in jsValue && jsValue["__value_type__"] == "ndarray") { return "ndarray"; }
-    if (typeof jsValue == "object") { return "dict"; }
+    if (typeof jsValue == "number" && Number.isInteger(jsValue)) { return ValueType.INT; }
+    if (typeof jsValue == "number") { return ValueType.FLOAT; }
+    if (typeof jsValue == "string") { return ValueType.STRING; }
+    if (typeof jsValue == "boolean") { return ValueType.BOOL; }
+    if (typeof jsValue == "object" && jsValue instanceof Table) { return ValueType.DATA_FRAME; }
+    if (typeof jsValue == "object" && jsValue.constructor == Uint8Array) { return ValueType.BYTES; }
+    if (typeof jsValue == "object" && jsValue.constructor == Array) { return ValueType.LIST; }
+    if (typeof jsValue == "object" && "__value_type__" in jsValue && jsValue["__value_type__"] == "ndarray") { return ValueType.NDARRAY; }
+    if (typeof jsValue == "object") { return ValueType.DICT; }
     throw new Error(`Value of type '${typeof jsValue}' could not be classified.`);
 }
 
@@ -19,27 +47,27 @@ export function getValueDescription(value: any, max_length?: number): string {
     if (max_length == undefined) { max_length = 80; }
     const type = getValueType(value);
 
-    if (type == "int") { return String(value); }
-    else if (type == "float") { return String(value); }
-    else if (type == "string") {
+    if (type == ValueType.INT) { return String(value); }
+    else if (type == ValueType.FLOAT) { return String(value); }
+    else if (type == ValueType.STRING) {
         if (value.length > max_length) { return value.substr(0, max_length) + " ..."; }
         else { return value; }
     }
-    else if (type == "bool") { return String(value); }
-    else if (type == "bytes") {
+    else if (type == ValueType.BOOL) { return String(value); }
+    else if (type == ValueType.BYTES) {
         return "";
     }
-    else if (type == "list") {
+    else if (type == ValueType.LIST) {
         return "Length: " + value.length;
     }
-    else if (type == "dict") {
+    else if (type == ValueType.DICT) {
         let desc = "Keys: [" + Object.keys(value).join(', ') + "]";
         return desc;
     }
-    else if (type == "data_frame") {
+    else if (type == ValueType.DATA_FRAME) {
         return "Length: " + value.length;
     }
-    else if (type == "ndarray") {
+    else if (type == ValueType.NDARRAY) {
         return "Shape: " + value.shape.join(', ') + " Type: " + value.type;
     }
     else {
@@ -54,7 +82,7 @@ export function valueHasKeyPath(value: any, keys: string[]): boolean {
     let subValue = value;
     for (let i=0; i<keys.length; i++) {
         const subValueType = getValueType(subValue);
-        if (subValueType != "dict" && subValueType != "list") { return false; }
+        if (subValueType != ValueType.DICT && subValueType != ValueType.LIST) { return false; }
         if (!(keys[i] in subValue)) { return false; }
         subValue = subValue[keys[i]];
     }
@@ -69,4 +97,34 @@ export function getSubValue(value: any, keys: string[]) {
         subValue = subValue[keys[i]];
     }
     return subValue;
+}
+
+
+export function legalStringToInt(value: string): boolean {
+    if (value === "") { return false; }
+    const number = Number(value);
+    return !isNaN(number) && Number.isInteger(number);
+}
+
+
+export function legalStringToFloat(value: string): boolean {
+    if (value === "") { return false; }
+    const number = Number(value);
+    return !isNaN(number);
+}
+
+
+export function stringToInt(value: string): number {
+    if (!legalStringToInt(value)) {
+        throw new Error(`Value '${value}' is not a legal integer.`);
+    }
+    return Number(value);
+}
+
+
+export function stringToFloat(value: string): number {
+    if (!legalStringToFloat(value)) {
+        throw new Error(`Value '${value}' is not a legal float.`);
+    }
+    return Number(value);
 }
